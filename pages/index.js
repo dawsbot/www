d6f5db0984e4770f503ed989149541a23eb1bc69
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2020-12-09 16:27:34 -0700

    crypto focus (#31)
    * More crypto focus
    
    * Homepage add youtube
    
    * fix eslint

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
-import { rehydrate } from 'glamor';
 
 import IndexIndex from '../src/components/Index/IndexIndex';
-
-// Adds server generated styles to glamor cache.
-// Has to run before any `css()` calls
-// '__NEXT_DATA__.ids' is set in '_document.js'
-if (typeof window !== 'undefined') {
-  // eslint-disable-next-line
-  rehydrate(window.__NEXT_DATA__.ids);
-}
 
 class Index extends Component {
   render() {
